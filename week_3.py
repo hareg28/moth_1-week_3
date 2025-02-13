d935556ab27b00d07a758820a27d739d5cecc9b1
haregewey <haregetewabe47@gmail.com>
2025-02-13 23:54:20 +0300

    week 3 task

---
+++ week_3.py
@@ -0,0 +1,42 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def describe(self):
+        return f"This is a {self.make} {self.model}."
+
+class Car(Vehicle):
+    def __init__(self, make, model, num_doors):
+        super().__init__(make, model)
+        self.__num_doors = num_doors 
+
+    def describe(self):
+        return f"This is a {self.make} {self.model} with {self.__num_doors} doors."
+    
+    def get_num_doors(self): 
+        return self.__num_doors
+
+class Bike(Vehicle):
+    def __init__(self, make, model, has_gears):
+        super().__init__(make, model)
+        self.has_gears = has_gears
+
+    def describe(self): 
+        gear_info = "with gears" if self.has_gears else "without gears"
+        return f"This is a {self.make} {self.model} {gear_info}."
+
+
+def vehicle_info(vehicle):
+    print(vehicle.describe())
+
+
+car = Car("Toyota", "Camry", 4)
+bike = Bike("Yamaha", "MT-07", True)
+
+
+vehicle_info(car)
+vehicle_info(bike)
+
+
+print(f"The car has {car.get_num_doors()} doors.")
